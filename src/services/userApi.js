0ada1ec5362665cd8cc54534fd14af3d85fee67f
Anna Rychkova <134734366+AnnaRychkova1@users.noreply.github.com>
2024-08-02 17:06:31 +0100

    Update userApi.js

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://phonebook-auth-api.onrender.com',
+  baseURL: 'https://phonebook-auth-api.onrender.com/api',
 });
 
 const setToken = token => {
